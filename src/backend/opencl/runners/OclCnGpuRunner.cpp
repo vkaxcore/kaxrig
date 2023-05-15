d9f8497bf44867e0cc25adfaa1d3b01e16a617c8
BenDr0id <ben@graef.in>
2023-05-15 18:24:44 +0200

    Dashboard changes
    -Changed sorting of column workerId to natural sorting
    -Fixed group by algo sorting, fixed offline notification banner
    -Added confirm dialog to "Reset ClientStatusList" #397
    -Added button to remove selected miner from list #396

--- src/backend/opencl/runners/OclCnGpuRunner.cpp
+++ src/backend/opencl/runners/OclCnGpuRunner.cpp
@@ -22,6 +22,7 @@
  *   along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdexcept>
 
 #include "backend/opencl/runners/OclCnGpuRunner.h"
 #include "backend/opencl/kernels/Cn00GpuKernel.h"
