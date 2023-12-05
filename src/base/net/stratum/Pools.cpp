b7d21a9181512179c001680c3766519e03df818d
BenDr0id <ben@graef.in>
2023-12-05 19:21:43 +0100

    Added missing header

--- src/base/net/stratum/Pools.cpp
+++ src/base/net/stratum/Pools.cpp
@@ -22,6 +22,7 @@
  *   along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <algorithm>
 
 #include "base/net/stratum/Pools.h"
 #include "3rdparty/rapidjson/document.h"
